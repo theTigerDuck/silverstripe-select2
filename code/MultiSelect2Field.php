0b4798a2a2e549fa48fbc768be6ca6ebca6686d2
Glen Ranson <glen@somar.co.nz>
2024-07-03 09:00:27 +1200

    Removing requirements that dont appear to work anyway and replacing with ones that do.

--- code/MultiSelect2Field.php
+++ code/MultiSelect2Field.php
@@ -15,10 +15,17 @@ class MultiSelect2Field extends ListBoxField
 {
     public function Field($properties = array())
     {
-        Requirements::javascript('silverstripe/admin: thirdparty/jquery/jquery.js');
-        Requirements::javascript('silverstripe/admin: thirdparty/jquery-entwine/dist/jquery.entwine-dist.js');
-        Requirements::javascript('sheadawson/silverstripe-select2: select2/select2.js');
-        Requirements::javascript('sheadawson/silverstripe-select2: javascript/ajaxselect2.init.js');
+
+        Requirements::javascript("https://code.jquery.com/jquery-3.7.1.min.js", [
+            'integrity' => 'sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=',
+            'crossorigin' => 'anonymous',
+        ]);
+
+        Requirements::javascript('silverstripe/admin: thirdparty/jquery-query/jquery.query.js');
+        Requirements::javascript('silverstripe/admin: thirdparty/jquery-entwine/jquery.entwine.js');
+
+        Requirements::javascript('sheadawson/silverstripe-select2: javascript/select2.init.js');
+
         Requirements::css('sheadawson/silverstripe-select2: select2/select2.css');
         $this->addExtraClass('select2')->addExtraClass('no-chzn');
 
