0aa82a30b08241b90cb648d670d8f0f891e0968b
Janosch Kilian <janosch@spleen.de>
2025-03-25 21:18:10 +0100

    Remove jquery conflicts

--- requirements/RequirementsTrait.php
+++ requirements/RequirementsTrait.php
@@ -11,13 +11,13 @@ trait RequirementsTrait
      */
     public function requireAppFiles(): void
     {
-        Requirements::javascript("https://code.jquery.com/jquery-3.7.1.min.js", [
-            'integrity' => 'sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=',
-            'crossorigin' => 'anonymous',
-        ]);
+//        Requirements::javascript("https://code.jquery.com/jquery-3.7.1.min.js", [
+//            'integrity' => 'sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=',
+//            'crossorigin' => 'anonymous',
+//        ]);
 
-        Requirements::javascript('silverstripe/admin: thirdparty/jquery-query/jquery.query.js');
-        Requirements::javascript('silverstripe/admin: thirdparty/jquery-entwine/jquery.entwine.js');
+//        Requirements::javascript('silverstripe/admin: thirdparty/jquery-query/jquery.query.js');
+//        Requirements::javascript('silverstripe/admin: thirdparty/jquery-entwine/jquery.entwine.js');
         Requirements::javascript('sheadawson/silverstripe-select2: javascript/select2.init.js');
         Requirements::javascript('sheadawson/silverstripe-select2: select2/select2.js');
         Requirements::javascript('sheadawson/silverstripe-select2: javascript/ajaxselect2.init.js');
